4d42c01a0631b697e9a900c04b193d2dcf779b1e
Cleo Aguiar <cleotaisas@gmail.com>
2022-04-21 14:05:59 -0300

    fix: Normalize data cholesterol and gluc

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -11,15 +11,16 @@ df['overweight'] = np.where((df['weight'] / (df['height']/100).pow(2)) > 25, 1, 
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 
-df['cholesterol'] = np.where((df['cholesterol'] - df['cholesterol'].min()) / (df['cholesterol'].max() - df['cholesterol'].min()) == 0, 1, 0)
+df.loc[df['cholesterol'] == 1, 'cholesterol'] = 0
+df.loc[df['cholesterol'] > 1, 'cholesterol'] = 1
 
-df['gluc'] = np.where((df['gluc'] - df['gluc'].min()) / (df['gluc'].max() - df['gluc'].min()) == 0, 1, 0)
+df['gluc'] = df['gluc'].apply(lambda x : 0 if x == 1 else 1)
 
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
     # df_cat = None
